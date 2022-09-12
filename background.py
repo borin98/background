a4730ebfde22b0686e4a600a4961a736878f3cd9
gabriel.macedo <gabriel.macedo@lnls.br>
2022-09-12 12:50:57 -0300

    Small changes to remove if the wallpaper was already chosen

--- background.py
+++ background.py
@@ -3,28 +3,49 @@ import os
 import time
 import glob
 import random
-class Wallpaper:
-    def __init__(self):
-        pass
+
+
+class Wallpaper(object):
+    def __init__(self, wallpaper_path: str):
+        self._wallpaper_path = wallpaper_path
+        self._wallpaper_list = glob.glob(wallpaper_path + "*.jpg") + glob.glob(wallpaper_path + "*.png")
+        self._wallpaper_list_copy = self._wallpaper_list.copy()
+        self._wallpaper_list_len = len(self._wallpaper_list)
+
+    def _remove_wallpaper_element(self, wallpaper_to_remove: str):
+        self._wallpaper_list.remove(wallpaper_to_remove)
+        self._wallpaper_list_len -= 1
 
     @staticmethod
-    def set_wallpaper():
-        # self.get_earthview()
-        path_vec = glob.glob("/home/gabriel_macedo/Documents/Wallpapers/*.jpg") + glob.glob("/home/gabriel_macedo/Documents/Wallpapers/*.png")
+    def _set_wallpaper(wallpaper_list: list[str]) -> str:
         secure_random = random.SystemRandom()
-        path_img = secure_random.choice(path_vec)
+        path_img = secure_random.choice(wallpaper_list)
+        print(len(path_img) * "=")
+        print("{} was chosen as wallpaper".format(path_img.split("/")[-1]))
+        print(len(path_img) * "=")
         cmd = "gsettings set org.gnome.desktop.background picture-uri-dark file://" + path_img
         os.system(cmd)
+        return path_img
+
+    def run_wallpaper(self):
+        chosen_wallpaper = self._set_wallpaper(self._wallpaper_list)
+        self._remove_wallpaper_element(chosen_wallpaper)
+
+        if (not self._wallpaper_list):
+            self._wallpaper_list = self._wallpaper_list_copy.copy()
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--time', help='Enter time in minutes')
+    parser.add_argument('-p', '--path', help="Enter the path to wallpapers")
 
     args = parser.parse_args()
     minutes = int(args.time)
+    files_path = args.path
+    wallpaper = Wallpaper(files_path)
     while True:
-        Wallpaper.set_wallpaper()
+        wallpaper.run_wallpaper()
         time.sleep(minutes * 60)
 
 
